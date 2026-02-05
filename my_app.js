e1f3db7353b10353ffebc7fe016f49f853df6e79
vasiliyBV <vasilii.b.v@mail.ru>
2026-02-05 13:55:19 +0300

    Update my_app.js

--- my_app.js
+++ my_app.js
@@ -1,8 +1,12 @@
 
 const button_array = document.getElementById('button_array');
 console.log(window.WebApp);
+console.log(WebApp);
+console.log(window);
+console.log(window.WebAppData);
+console.log(WebAppData);
 const BackButton = window.WebApp.BackButton;
-const userName = window.WebAppData.user.username;
+const userName = 'test';
 document.querySelector('#hi').textContent = 'Добро пожаловать, '+ userName + '.';
 const Userid=WebAppData.user.id;
 const links = {
@@ -47,6 +51,7 @@ BackButton.onClick()
 	BackButton.hide(); 	
 };
 window.WebApp.ready();
+
 
 
 
